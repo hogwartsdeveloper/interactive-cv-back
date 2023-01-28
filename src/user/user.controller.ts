33c705ff22dfbcddd5ffbd2cc09ba400f2232c9d
zhannur <jannuraidynuly@gmail.com>
2023-01-28 17:42:43 +0600

    login jsonwebtoken

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,12 +6,18 @@ import { types } from '../types';
 import { ILogger } from '../logger/logger.interface';
 import { IUserService } from './service/user.service.interface';
 import { HttpError } from '../error/http-error.class';
+import { ValidateMiddleware } from '../comman/validate.middleware';
+import { UserRegisterDto } from './dto/user-register.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { sign } from 'jsonwebtoken';
+import { IConfigService } from '../config/config.service.interface';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
 	constructor(
 		@inject(types.Logger) private loggerService: ILogger,
 		@inject(types.UserService) private userService: IUserService,
+		@inject(types.ConfigService) private configService: IConfigService,
 	) {
 		super(loggerService);
 		this.bindRoutes([
@@ -19,11 +25,13 @@ export class UserController extends BaseController implements IUserController {
 				path: '/register',
 				method: 'post',
 				func: this.register,
+				middlewares: [new ValidateMiddleware(UserRegisterDto)],
 			},
 			{
 				path: '/login',
 				method: 'post',
 				func: this.login,
+				middlewares: [new ValidateMiddleware(UserLoginDto)],
 			},
 			{
 				path: '/info',
@@ -36,16 +44,49 @@ export class UserController extends BaseController implements IUserController {
 		this.ok(res, 'Info');
 	}
 
-	login(req: Request, res: Response, next: NextFunction): void {
-		this.ok(res, 'Login');
+	async login(
+		{ body }: Request<{}, {}, UserLoginDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const result = await this.userService.validate(body);
+		if (!result) {
+			return next(new HttpError(401, 'User does not exist'));
+		}
+		const jwt = await this.signJWT(body.email, this.configService.get('SECRET'));
+
+		this.ok(res, { jwt });
 	}
 
-	async register({ body }: Request, res: Response, next: NextFunction): Promise<void> {
+	async register(
+		{ body }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
 		const result = await this.userService.create(body);
 		if (!result) {
 			return next(new HttpError(422, 'User exist'));
 		}
 
 		this.ok(res, { email: result.email, id: result.id });
+	}
+
+	private signJWT(email: string, secret: string): Promise<string> {
+		return new Promise<string>((res, rej) => {
+			sign(
+				{
+					email,
+					iat: Math.floor(Date.now() / 1000),
+				},
+				secret,
+				{ algorithm: 'HS256' },
+				(err, token) => {
+					if (err) {
+						rej(err);
+					}
+					res(token as string);
+				},
+			);
+		});
 	}
 }
