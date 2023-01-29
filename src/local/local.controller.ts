f80c1ab748ffcddce8191b54b29a83f41991c1e1
zhannur <jannuraidynuly@gmail.com>
2023-01-29 22:35:38 +0600

    local swagger doc

--- src/local/local.controller.ts
+++ src/local/local.controller.ts
@@ -32,6 +32,63 @@ export class LocalController extends BaseController implements ILocalController 
 		]);
 	}
 
+	/**
+	 * @swagger
+	 * /local:
+	 *   post:
+	 *     description: Add local
+	 *     tags: [Local]
+	 *     produces:
+	 *       - application/json
+	 *     requestBody:
+	 *        required: true
+	 *        content:
+	 *          application/json:
+	 *            schema:
+	 *              type: object
+	 *              properties:
+	 *                code:
+	 *                  type: string
+	 *                  description: The local code.
+	 *                name:
+	 *                  type: object
+	 *                  properties:
+	 *                    en:
+	 *                      type: string
+	 *                    kz:
+	 *                      type: string
+	 *                    ru:
+	 *                      type: string
+	 *     responses:
+	 *       200:
+	 *         description: registration
+	 *         content:
+	 *            application/json:
+	 *              schema:
+	 *                type: object
+	 *                properties:
+	 *                  id:
+	 *                    type: integer
+	 *                    description: The local id
+	 *                  code:
+	 *                    type: string
+	 *                    description: The local code
+	 *                  name:
+	 *                    type: object
+	 *                    properties:
+	 *                      id:
+	 *                        type: integer
+	 *                      en:
+	 *                        type: string
+	 *                      kz:
+	 *                        type: string
+	 *                      ru:
+	 *                        type: string
+	 *                      localId:
+	 *                        type: integer
+	 *     security:
+	 *       - bearerAuth: []
+	 */
 	async add(
 		{ body }: Request<{}, {}, LocalCreateDto>,
 		res: Response,
@@ -44,6 +101,44 @@ export class LocalController extends BaseController implements ILocalController 
 		this.ok(res, { id: result.id, code: result.code, name: result?.name });
 	}
 
+	/**
+	 * @swagger
+	 * /local:
+	 *   get:
+	 *     description: Get all local
+	 *     tags: [Local]
+	 *     produces:
+	 *       - application/json
+	 *     responses:
+	 *       200:
+	 *         description: info
+	 *         content:
+	 *            application/json:
+	 *              schema:
+	 *                type: array
+	 *                items:
+	 *                  type: object
+	 *                  properties:
+	 *                    id:
+	 *                      type: integer
+	 *                      description: The local id
+	 *                    code:
+	 *                      type: string
+	 *                      description: The local code
+	 *                    name:
+	 *                      type: object
+	 *                      properties:
+	 *                        id:
+	 *                          type: integer
+	 *                        en:
+	 *                          type: string
+	 *                        kz:
+	 *                          type: string
+	 *                        ru:
+	 *                          type: string
+	 *                        localId:
+	 *                          type: integer
+	 */
 	async get(req: Request, res: Response, next: NextFunction): Promise<void> {
 		const result = await this.localService.getAll();
 		this.ok(res, { result });
