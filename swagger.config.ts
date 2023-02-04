88f3d178ff82ad0f781a2e1908d5796118c93b37
zhannur <jannuraidynuly@gmail.com>
2023-02-04 17:20:04 +0600

    add jest

--- swagger.config.ts
+++ swagger.config.ts
@@ -1,23 +1,23 @@
-import { Options } from 'swagger-jsdoc';
+import { Options } from "swagger-jsdoc";
 
 export const options: Options = {
-	swaggerDefinition: {
-		openapi: '3.0.0',
-		info: {
-			title: 'My API',
-			version: '1.0.0',
-			description: 'A sample API',
-		},
-		basePath: '/swagger',
-		components: {
-			securitySchemes: {
-				bearerAuth: {
-					type: 'http',
-					scheme: 'bearer',
-					bearerFormat: 'JWT',
-				},
-			},
-		},
-	},
-	apis: ['./src//**/*controller.ts'],
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "CV API",
+      version: "1.0.0",
+      description: "sumurai.kz",
+    },
+    basePath: "/swagger",
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+  },
+  apis: ["./src//**/*controller.ts"],
 };
