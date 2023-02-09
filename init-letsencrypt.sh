7b9cce2efe2ad5dfa1611cc8210504316a1e81c3
zhannur <jannuraidynuly@gmail.com>
2023-02-09 19:26:43 +0600

    fix

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(back.sumurai.kz www.back.sumurai.kz)
+domains=(back.sumurai.kz)
 rsa_key_size=4096
 data_path="./certbot"
 email="jannuraidynuly@gmail.com" # Adding a valid address is strongly recommended
